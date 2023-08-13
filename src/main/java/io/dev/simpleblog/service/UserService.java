b8ce79e08afb3b6aabb385f49c45ee2f6803160e
deneb1593 <deneb1593@gmail.com>
2023-08-14 01:50:36 +0900

    (add): 회원 조회 by 이메일

--- src/main/java/io/dev/simpleblog/service/UserService.java
+++ src/main/java/io/dev/simpleblog/service/UserService.java
@@ -6,6 +6,7 @@ import io.dev.simpleblog.domain.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,6 +28,15 @@ public class UserService {
             passwordEncoder.encode(userDto.password())
         );
         return userRepository.save(user).toDto();
+    }
+
+    @Transactional(readOnly = true)
+    public UserDto findUserByEmail(String email) {
+        return userRepository.findByEmail(email)
+            .orElseThrow(
+                () -> new UsernameNotFoundException(
+                    String.format("user not found [email: %s]", email)))
+            .toDto();
     }
 
 }
--- src/main/java/io/dev/simpleblog/web/UserController.java
+++ src/main/java/io/dev/simpleblog/web/UserController.java
@@ -26,5 +26,4 @@ public class UserController {
             userService.join(userDto));
     }
 
-
 }
